d0cb4129f05b862bda5b53507e88b81c51e5c2c8
damianklata1 <costam123987@gmail.com>
2022-12-06 15:22:14 +0100

    Struktura

---
+++ project_ejb/ejbModule/com/jsf/entities/User.java
@@ -0,0 +1,140 @@
+package com.jsf.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idUser;
+
+	private String active;
+
+	@Column(name="`e-mail`")
+	private String e_mail;
+
+	private String login;
+
+	private String name;
+
+	private String password;
+
+	private String surname;
+
+	//bi-directional many-to-many association to Role
+	@ManyToMany
+	@JoinTable(
+		name="roleofuser"
+		, joinColumns={
+			@JoinColumn(name="idUser")
+			}
+		, inverseJoinColumns={
+			@JoinColumn(name="idRole")
+			}
+		)
+	private List<Role> roles;
+
+	//bi-directional many-to-one association to Transaction
+	@OneToMany(mappedBy="user")
+	private List<Transaction> transactions;
+
+	public User() {
+	}
+
+	public int getIdUser() {
+		return this.idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getActive() {
+		return this.active;
+	}
+
+	public void setActive(String active) {
+		this.active = active;
+	}
+
+	public String getE_mail() {
+		return this.e_mail;
+	}
+
+	public void setE_mail(String e_mail) {
+		this.e_mail = e_mail;
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public List<Role> getRoles() {
+		return this.roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public List<Transaction> getTransactions() {
+		return this.transactions;
+	}
+
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
+	public Transaction addTransaction(Transaction transaction) {
+		getTransactions().add(transaction);
+		transaction.setUser(this);
+
+		return transaction;
+	}
+
+	public Transaction removeTransaction(Transaction transaction) {
+		getTransactions().remove(transaction);
+		transaction.setUser(null);
+
+		return transaction;
+	}
+
+}
